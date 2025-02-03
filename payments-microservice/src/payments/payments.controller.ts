5f5405ccacb9f8493e3898b8db5c5400a32597a4
MarkusTech <wm.recopelacion@gmail.com>
2025-02-03 16:39:11 +0800

    payment and user microservices can now talk to each other  or the user can now see the emit data from payment microservice and the users will use EventPattern to read the event from payment

--- payments-microservice/src/payments/payments.controller.ts
+++ payments-microservice/src/payments/payments.controller.ts
@@ -1,11 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy, EventPattern, Payload } from '@nestjs/microservices';
 import { CreatePaymentDto } from './dto/CreatePayment.dto';
 
 @Controller()
 export class PaymentsMicroserviceController {
+  constructor(@Inject('NATS_SERVICE') private natsClient: ClientProxy) {} // this is from the nats module in payment-microservices so that payment and user can connect to each other
+
   @EventPattern('createPayment')
   createPayment(@Payload() createPaymentDto: CreatePaymentDto) {
     console.log(createPaymentDto);
+    this.natsClient.emit('paymentCreated', createPaymentDto);
   }
 }
