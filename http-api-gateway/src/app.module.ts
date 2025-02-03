83b7bb1df7fb4dc3dd1f5b088116da5deb56714a
MarkusTech <wm.recopelacion@gmail.com>
2025-02-03 15:16:58 +0800

    Update app.module.ts

--- http-api-gateway/src/app.module.ts
+++ http-api-gateway/src/app.module.ts
@@ -1,10 +1,10 @@
 import { NatsClientModule } from 'src/nats-clients/nats-client.module';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { PaymentModule } from './payments/payments.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [NatsClientModule, UsersModule, PaymentModule],
+  imports: [NatsClientModule, UsersModule, PaymentsModule],
   controllers: [],
   providers: [],
 })
