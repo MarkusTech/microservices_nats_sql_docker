40fd6a5f41dfbcee60273c1f66877af7d220cb0b
MarkusTech <wm.recopelacion@gmail.com>
2025-02-03 16:26:10 +0800

    EventPattern

--- payments-microservice/src/payments/payments.controller.ts
+++ payments-microservice/src/payments/payments.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { CreatePaymentDto } from './dto/CreatePayment.dto';
 
-@Controller('payments')
-export class PaymentsMicroserviceController {}
+@Controller()
+export class PaymentsMicroserviceController {
+  @EventPattern('createPayment')
+  createPayment(@Payload() createPaymentDto: CreatePaymentDto) {
+    console.log(createPaymentDto);
+  }
+}
